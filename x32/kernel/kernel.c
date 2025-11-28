ffd58cfd55d80838b2be3a7fcc2aced9a763e740
Sskame1 <tolikstrgonov111@gmail.com>
2025-11-28 17:25:57 +0900

    added func clear_screen

--- x32/kernel/kernel.c
+++ x32/kernel/kernel.c
@@ -1,4 +1,15 @@
-void kernel_main() {
+void clear_screen() {
+    volatile unsigned short *video_memory = (volatile unsigned short *)0xB8000;
+    int screen_size = 80 * 25;
     
+    unsigned short cell = 0x20 | (0x27 << 8);
+    for (int i = 0; i < screen_size; i++) {
+        video_memory[i] = cell;
+    }
+}
+
+void kernel_main() {
+    clear_screen();
+
     while(1);
 }
