b6c30a7528f92c6ae41388aa155a0821b4717749
Sskame1 <tolikstrgonov111@gmail.com>
2025-12-04 23:02:09 +0900

    Написал простенький планировщик в kernel.c

--- x32/kernel/core/kernel.c
+++ x32/kernel/core/kernel.c
@@ -1,4 +1,17 @@
 #include "drivers/video/screen.h"
+#include "modules/module.h"
+#include "ipc/ipc.h"
+
+// простенький планировщик - round-robin
+typedef struct {
+    int pid;
+    int status;
+    void *stack_pointer //0 - готов 1 - работает 2 - заблокирован
+} Task;
+
+static Task tasks[32];
+static int current_task = 0;
+static int task_count = 0;
 
 void kernel_main() {
     clear_screen();
